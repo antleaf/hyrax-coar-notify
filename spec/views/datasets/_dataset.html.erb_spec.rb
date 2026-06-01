de4fdb1925bc3c740ea8531a4f710f76fd458ed4
Gyan Gupta <gyan@cottagelabs.com>
2026-06-01 11:39:17 +0530

    implemented new resource dataset

---
+++ spec/views/datasets/_dataset.html.erb_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work_resource Dataset`
+RSpec.describe 'datasets/dataset.html.erb', type: :view do # rubocop:disable  RSpec/EmptyExampleGroup
+  # add dataset view specs
+end
